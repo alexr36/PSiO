fe6353c1d2ff1b4acbfaa54589726603933bd056
Alex Rogoziński <39859634+alexr36@users.noreply.github.com>
2025-07-23 09:51:17 +0200

    Update Układanka.java

--- Cwiczenia/List1/Układanka.java
+++ Cwiczenia/List1/Układanka.java
@@ -1,7 +1,7 @@
 // 5. Wykonaj zadanie: (Rusz Głową Java, wydanie 2, Rozdział 1 - str. 52):
 //
-//	  Magnesiki z kodem
-//	  Działający program Javy został podzielony na fragmenty. Czy jesteś w stanie złożyć
+//    Magnesiki z kodem
+//    Działający program Javy został podzielony na fragmenty. Czy jesteś w stanie złożyć
 //    go z powrotem w jedną całość, tak aby wygenerował przedstawione poniżej wyniki?
 //    Niektóre nawiasy klamrowe spadły na podłogę i były zbyt małe, aby można je było
 //    podnieść, dlatego w razie potrzeby możesz je dodawać!
@@ -9,14 +9,14 @@
 // Kod w odpowiedniej kolejności z uzupełnionymi nawiasami:
 class Układanka {								// Fragment 3.
 	public static void main(String[] args) {
-		int x = 3;								// Fragment 5.
+		int x = 3;							// Fragment 5.
 		
 		while (x > 0) {							// Fragment 7.
 			if (x > 2) {						// Fragment 4.
 				System.out.print("a");
 			}
 
-			x = x - 1;							// Fragment 6.
+			x = x - 1;						// Fragment 6.
 			System.out.print("-");
 
 			if (x == 2) {						// Fragment 2.
